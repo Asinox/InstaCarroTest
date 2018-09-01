9dc3010622996d09af005dc5282494181e4860d3
Juan Miguel Calcano <asinox@gmail.com>
2018-08-31 23:42:58 -0400

    removing some js files

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-//import Moment from 'react-moment';
 import  Timer from "react-time-counter"
 
 export default class Listing extends Component{
@@ -23,25 +22,6 @@ export default class Listing extends Component{
     formatKm(km){
         let k = km / 1000;
         return k.toFixed(1);
-    }
-
-    displayTime(millisec) {
-        var seconds = (millisec / 1000).toFixed(0);
-        var minutes = Math.floor(seconds / 60);
-        var hours = "";
-        if (minutes > 59) {
-            hours = Math.floor(minutes / 60);
-            hours = (hours >= 10) ? hours : "0" + hours;
-            minutes = minutes - (hours * 60);
-            minutes = (minutes >= 10) ? minutes : "0" + minutes;
-        }
-
-        seconds = Math.floor(seconds % 60);
-        seconds = (seconds >= 10) ? seconds : "0" + seconds;
-        if (hours !== "") {
-            return hours + ":" + minutes + ":" + seconds;
-        }
-        return minutes + ":" + seconds;
     }
 
     componentDidMount(){
